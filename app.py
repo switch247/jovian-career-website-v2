163aebe0a6658b1b345063dec2e0de90bbd52313
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-07-11 13:44:51 +0300

    reqs added

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS =[{
+  'id':1,
+  'title': 'Data Analyst',
+  'Location': 'Delhi, India',
+  'salary': 'br. 12,000'
+},
+{
+ 'id':2,
+  'title': 'Data scientist',
+  'Location': 'Delhi, India',
+  'salary': 'br. 12,000'
+}
+,
+{
+ 'id':3,
+  'title': 'Front End Developer',
+  'Location': 'Delhi, India',
+}
+]
 
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html",jobs=JOBS,comp_name='Jovian')
 
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
